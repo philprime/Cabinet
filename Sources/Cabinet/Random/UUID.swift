88e698e586220bded644ed4bed703d2c3308311e
Philip Niedertscheider <dev@philpri.me>
2020-02-21 21:33:09 +0100

    Added UUID

---
+++ Sources/Cabinet/Random/UUID.swift
@@ -0,0 +1,22 @@
+//
+//  UUID.swift
+//  Cabinet
+//
+//  Created by Philip Niedertscheider on 21.02.20.
+//  Copyright © 2020 Philip Niedertscheider. All rights reserved.
+//
+
+import Darwin
+
+public struct UUID {
+    
+    public let uuidString: String
+    
+    public init() {
+        var uu = [UInt8](repeating: 0, count: 16)
+        uuid_generate(&uu)
+        var out = [Int8](repeating: 8, count: 36)
+        uuid_unparse(&uu, &out)
+        uuidString = String(bytes: out.map(UInt8.init), encoding: String.Encoding.utf8)!
+    }
+}
