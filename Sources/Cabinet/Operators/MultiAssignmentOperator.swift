673ae9aa2ca444c609d3026f0ed513888885081c
Philip Niedertscheider <dev@philpri.me>
2021-04-02 23:12:31 +0200

    Fixed multi assignment operator casting order

--- Sources/Cabinet/Operators/MultiAssignmentOperator.swift
+++ Sources/Cabinet/Operators/MultiAssignmentOperator.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2020 Philip Niedertscheider. All rights reserved.
 //
 
+// swiftlint:disable static_operator
+
 /// Operator for assigning and returning a value
 infix operator <--: MultiAssignmentOperatorPrecedenceGroup
 /// Operator for assigning and returning a value, if it is `nil`
@@ -16,9 +18,32 @@ infix operator <-?-: MultiAssignmentOperatorPrecedenceGroup
 ///   - target: Pointer to where to store the given value
 ///   - value: Value to be set
 /// - Returns: Value for direct usage
-public func <-- <T>(target: inout T, value: T) -> T {
-    target = value
-    return value
+public func <-- <T>(target: inout T?, value: @autoclosure () -> T?) -> T? {
+    let val = value()
+    target = val
+    return val
+}
+
+/// Assigns parameter `value` into`target` and returns `value` afterwards to be used again
+/// - Parameters:
+///   - target: Pointer to where to store the given value
+///   - value: Value to be set
+/// - Returns: Value for direct usage
+public func <-- <T>(target: inout T, value: @autoclosure () -> T) -> T {
+    let val = value()
+    target = val
+    return val
+}
+
+/// Assigns parameter `value` into`target` and returns `value` afterwards to be used again
+/// - Parameters:
+///   - target: Pointer to where to store the given value
+///   - value: Value to be set
+/// - Returns: Value for direct usage
+public func <-- <T>(target: inout T?, value: @autoclosure () -> T) -> T {
+    let val = value()
+    target = val
+    return val
 }
 
 /// Assigns parameter `value` into`target` if target is `nil` and returns `value` in any case
@@ -38,7 +63,8 @@ precedencegroup MultiAssignmentOperatorPrecedenceGroup {
     // We start with evaluating the value and assigning afterwards into the left variable
     associativity: right
 
-    // An input value should be casted first, before actually assigning to another variable
+    assignment: true
+
     lowerThan: CastingPrecedence
 
 }
