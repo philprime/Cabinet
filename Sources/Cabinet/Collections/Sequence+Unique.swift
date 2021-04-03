12cc9ae3f005ad4d887f1248144e82e1e4935a7b
Philip Niedertscheider <dev@philpri.me>
2021-04-03 17:12:47 +0200

    Added Sequence + Unique

---
+++ Sources/Cabinet/Collections/Sequence+Unique.swift
@@ -0,0 +1,54 @@
+extension Sequence {
+
+    /// Returns a sequence with duplicate elements removed, performing the comparison usinig the property at
+    /// the supplied keypath.
+    ///
+    /// i.e.
+    ///
+    /// ```
+    /// [
+    ///   MyStruct(value: "Hello"),
+    ///   MyStruct(value: "Hello"),
+    ///   MyStruct(value: "World")
+    ///  ].uniqued(\.value)
+    /// ```
+    /// would result in
+    ///
+    /// ```
+    /// [
+    ///   MyStruct(value: "Hello"),
+    ///   MyStruct(value: "World")
+    /// ]
+    /// ```
+    ///
+    /// - note: Source: stackoverflow.com/a/46354989/3141234
+    ///
+    public func unique<T: Equatable>(by keyPath: KeyPath<Element, T>) -> [Element] {
+        self.unique(by: { $0[keyPath: keyPath] == $1[keyPath: keyPath] })
+    }
+}
+
+extension Sequence {
+
+    /// Return the sequence with all duplicates removed.
+    ///
+    /// Duplicate, in this case, is defined as returning `true` from `comparator`.
+    ///
+    /// - Parameter comparator: A comparison closure. `comparator` accepts two elements of this sequence as its parameter and returns a truthy boolean if the two elements have the same identifier
+    /// - Returns: A sequence containing only elements uniquely identified by the given comparator
+    /// - Note: Source: stackoverflow.com/a/46354989/3141234
+    public func unique(by comparator: @escaping (Element, Element) throws -> Bool) rethrows -> [Element] {
+        var buffer: [Element] = []
+
+        for element in self {
+            // If element is already in buffer, skip to the next element
+            if try buffer.contains(where: { try comparator(element, $0) }) {
+                continue
+            }
+
+            buffer.append(element)
+        }
+
+        return buffer
+    }
+}
---
+++ Tests/CabinetTests/Collections/Sequence_UniqueSpec.swift
@@ -0,0 +1,78 @@
+import Quick
+import Nimble
+@testable import Cabinet
+
+class Sequence_UniqueSpec: QuickSpec {
+
+    override func spec() {
+        describe("Sequence") {
+
+            struct TestObject: Equatable {
+                let id: String
+            }
+
+            describe("unique by keypath") {
+                it("should return same as comparing properties") {
+                    let sequence = [
+                        TestObject(id: "id-1"),
+                        TestObject(id: "id-1"),
+                        TestObject(id: "id-3"),
+                    ]
+                    expect(sequence.unique(by: \.id)) == sequence.unique(by: { lhs, rhs in
+                        lhs.id == rhs.id
+                    })
+                }
+            }
+
+            describe("unique by comparator") {
+                context("empty sequence") {
+                    it("should return empty sequence") {
+                        let sequence: [TestObject] = []
+                        expect(sequence.unique(by: \.id)).to(beEmpty())
+                    }
+                }
+
+                context("unique values") {
+                    it("should return same sequence") {
+                        let sequence = [
+                            TestObject(id: "id-1"),
+                            TestObject(id: "id-2"),
+                            TestObject(id: "id-3"),
+                        ]
+                        expect(sequence.unique(by: \.id)) == sequence
+                    }
+                }
+
+                context("duplicate identifiers") {
+                    it("should remove duplicates") {
+                        let sequence = [
+                            TestObject(id: "id-1"),
+                            TestObject(id: "id-1"),
+                            TestObject(id: "id-3"),
+                        ]
+                        expect(sequence.unique(by: \.id)) == [
+                            TestObject(id: "id-1"),
+                            TestObject(id: "id-3"),
+                        ]
+                    }
+
+                    it("should return first occurance") {
+                        struct AnotherTestObject: Equatable {
+                            let id: String
+                            let value: String
+                        }
+                        let sequence = [
+                            AnotherTestObject(id: "id-1", value: "value-1"),
+                            AnotherTestObject(id: "id-1", value: "value-2"),
+                            AnotherTestObject(id: "id-3", value: "value-3"),
+                        ]
+                        expect(sequence.unique(by: \.id)) == [
+                            AnotherTestObject(id: "id-1", value: "value-1"),
+                            AnotherTestObject(id: "id-3", value: "value-3"),
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}
