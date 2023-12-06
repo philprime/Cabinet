2d4dad2481ff932739d159d51d9843d0dc4e48f5
Philip Niedertscheider <me@philprime.dev>
2023-12-06 11:28:59 +0100

    fix: removed auto-describing injecting overwriting too much

--- Sources/CabinetCore/Logging/CustomStringConvertible+AutoDescribing.swift
+++ Sources/CabinetCore/Logging/CustomStringConvertible+AutoDescribing.swift
@@ -1,16 +1,7 @@
-extension CustomStringConvertible {
-
+public extension CustomStringConvertible {
     /// Default implementation creating a description using reflection
-    public var description: String {
-        return createReflectionString(of: self)
-    }
-}
-
-extension CustomDebugStringConvertible {
-
-    /// Default implementation creating a description using reflection
-    public var debugDescription: String {
-        return createReflectionString(of: self)
+    var autoDescription: String {
+        createReflectionString(of: self)
     }
 }
 
@@ -18,11 +9,11 @@ private func createReflectionString(of object: Any) -> String {
     let typeString = String(describing: type(of: object))
     let address = Unmanaged.passUnretained(object as AnyObject).toOpaque().debugDescription
     let children = Mirror(reflecting: object).children
-        .compactMap({ child -> (label: String, value: Any)? in
+        .compactMap { child -> (label: String, value: Any)? in
             guard let propertyName = child.label else { return nil }
             return (label: propertyName, value: child.value)
-        })
-        .map({ "\($0.label): \($0.value)" })
+        }
+        .map { "\($0.label): \($0.value)" }
         .joined(separator: ", ")
 
     return String(format: "%@<%@>(%@)", typeString, address, children)
