6bcd6aee4217cd78149e14c8c548a517c46ddce3
Philip Niedertscheider <me@philprime.dev>
2023-12-06 12:00:08 +0100

    fix: removed auto-describing injecting overwriting too much and causing crashes (#16)
    * fix: removed auto-describing injecting overwriting too much
    
    * WIP

--- Tests/CabinetCoreTests/Logging/CustomStringConvertible_AutoDescribingSpec.swift
+++ Tests/CabinetCoreTests/Logging/CustomStringConvertible_AutoDescribingSpec.swift
@@ -5,12 +5,17 @@ class CustomStringConvertible_AutoDescribingSpec: XCTestCase {
 
     func testCustomStringConvertible_shouldContainAllFields() {
         class Question: CustomStringConvertible {
+
             let number: Int
             let text: String
 
             init(number: Int, text: String) {
                 self.number = number
                 self.text = text
+            }
+
+            var description: String {
+                autoDescription
             }
         }
 
@@ -29,11 +34,15 @@ class CustomStringConvertible_AutoDescribingSpec: XCTestCase {
                 self.number = number
                 self.text = text
             }
+
+            var description: String {
+                autoDescription
+            }
         }
 
         let question = Question(number: 42, text: "is the answer")
 
-        XCTAssertTrue(question.description.hasPrefix("Question<"))
-        XCTAssertTrue(question.description.hasSuffix(">(number: 42, text: is the answer)"))
+        XCTAssertTrue(question.autoDescription.hasPrefix("Question<"))
+        XCTAssertTrue(question.autoDescription.hasSuffix(">(number: 42, text: is the answer)"))
     }
 }
